72a076d0954735add213f3d8c9c045cbd532602d
howevert <1413537501@qq.com>
2020-06-15 11:20:07 +0800

    finsh index

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+console.log(name.toUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var result=sentence.split(" ");
+var fin="";
+for (var i=0;i<result.length;i++) {
+    fin+=result[i].charAt(0).toUpperCase()+result[i].substr(1,result[i].length-1)+" ";
+}
+console.log(fin);
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+console.log(money.substring(1,3));
